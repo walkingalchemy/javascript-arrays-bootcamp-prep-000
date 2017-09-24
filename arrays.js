c0390d09bbe5037b51724dfe452d945ecd8293f9
James Royer <walkingalchemy@gmail.com>
2017-09-24 15:08:54 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,7 @@ var chocolateBars = [
 ]
 
 function addElementToBeginningOfArray(array, element) {
-  array.unshift(element)
+  return [element, ...array]
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
