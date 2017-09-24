551994d10458f7f8d6f7a3321a7e82e56f6177a7
James Royer <walkingalchemy@gmail.com>
2017-09-24 15:14:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -13,3 +13,13 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
   return array
 }
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+}
+
