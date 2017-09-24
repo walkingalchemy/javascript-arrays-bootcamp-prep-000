51a0903aa840150a730a9e2239c9ce72956d707e
James Royer <walkingalchemy@gmail.com>
2017-09-24 15:32:26 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -40,3 +40,7 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop()
   return array
 }
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length -1)
+}
